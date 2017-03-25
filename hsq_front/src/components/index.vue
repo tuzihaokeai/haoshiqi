19b34b01f7967f1b81d4b11582e6dbf82a1b20ac
Annie <822153621@qq.com>
2017-03-25 14:35:47 +0800

    index 静态页面1

--- hsq_front/src/components/index.vue
+++ hsq_front/src/components/index.vue
@@ -1,10 +1,10 @@
 <template>
 	<div id="index">
 		<div class="portal-header">
-      <div class="portal-logo"></div>
-      <input type="text" placeholder="搜索您想要找的商品">
-      <div class="portal-location J_go_city">上海市<span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span></div>
+      <span  class="portal-logo"></span><input type="text" placeholder="搜索您想要找的商品" class="search"/>
+      <div class="portal-location">上海市<span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span></div>
     	</div>
+    	<div class="banner"></div>
 		<router-view></router-view>
 	</div>
 </template>
@@ -20,9 +20,42 @@ import css from '../bootstrap/css/bootstrap.css'
 	}
 </script>
 <style scoped>
-	.iconfont{
-		width: 10px;
-		height: 10px;
-		color: #000000;	
+	.portal-header{
+	height: 50px;
+    padding:10px 80px;
+    background: #fe1;
+	}
+	.portal-logo{
+	position: absolute;
+    top: 15px;
+    left: 5px;
+    width: 70px;
+    height: 22px;
+    background: url(../assets/logo.png) top left no-repeat;
+    background-size: auto 100%;
+	}
+	.search{
+	display: block;
+    width: 100%;
+    height: 100%;
+    padding: 10px;
+    line-height: 30px;
+    border: none;
+    background: #fff;
+    border-radius: 5px;
+    font-size: 16px;
+	}
+	.portal-location{
+	position: absolute;
+    top: 15px;
+    right: 15px;
+    color: #914e07;
+    font-weight: 700;
+    z-index: 1000
+	}
+	.banner{
+		background: url(../assets/banner.jpg) center no-repeat;
+		height: 210px;
+		background-size: 100%;
 	}
 </style>
