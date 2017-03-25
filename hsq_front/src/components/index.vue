5c5a6e8f8738e7ba7362552f6319c5aff6e55ef6
Annie <822153621@qq.com>
2017-03-25 11:06:44 +0800

    bootstrap.css

--- hsq_front/src/components/index.vue
+++ hsq_front/src/components/index.vue
@@ -1,10 +1,16 @@
 <template>
 	<div id="index">
-		index
+		<div class="portal-header">
+      <div class="portal-logo"></div>
+      <input type="text" placeholder="搜索您想要找的商品">
+      <div class="portal-location J_go_city">上海市<span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span></div>
+    	</div>
 		<router-view></router-view>
 	</div>
 </template>
 <script>
+import css from '../bootstrap/css/bootstrap.css'
+
 	export default {
 		data(){
 			return{
@@ -14,5 +20,9 @@
 	}
 </script>
 <style scoped>
-	
+	.iconfont{
+		width: 10px;
+		height: 10px;
+		color: #000000;	
+	}
 </style>
