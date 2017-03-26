2167a418f6e67f498c8a91a41a588ced03bf2b84
Annie <822153621@qq.com>
2017-03-26 22:33:13 +0800

    index-list静态页面

--- hsq_front/src/components/list.vue
+++ hsq_front/src/components/list.vue
@@ -13,6 +13,57 @@
     		</li>
     		</ul>
     	</div>
+    	<ul class="extension-item">
+    		<li><img src="../assets/1.jpg"/></li>
+    		<li><img src="../assets/2.jpg"/></li>
+    		<li><img src="../assets/3.jpg"/></li>
+    		<li><img src="../assets/4.jpg"/></li>
+    		<li><img src="../assets/5.jpg"/></li>
+    	</ul>
+    	<ul class="index_list">
+    		<li>
+    			<img src="../assets/list1.jpg"/>
+    			<div class="index_text">
+				<h3>秒杀 马来西亚桂格燕麦饼干一盒162g口味随机</h3>
+				<p>
+					<span class="tag" style="background: #FF5555;">4.7折</span>
+				  <dfn class="index_price">¥ <span class="price_box">7.80</span></dfn>
+				  <del class="del_price">8.90</del>
+				</p>
+					<p class="shopcar"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></p>
+				<p>
+
+				  </p>
+  				</div>
+    		</li>
+    		<li>
+    			<img src="../assets/list1.jpg"/>
+    			<div class="index_text">
+				<h3>秒杀 马来西亚桂格燕麦饼干一盒162g口味随机</h3>
+				<p>
+					<span class="tag" style="background: #FF5555;">4.7折</span>
+				  <dfn class="index_price">¥ <span class="price_box">7.80</span></dfn>
+				  <del class="del_price">8.90</del>
+				</p>
+					<p class="shopcar"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></p>
+				<p>
+
+				  </p>
+  				</div>
+    		</li>
+    		<li>
+    			<img src="../assets/list1.jpg"/>
+    			<div class="index_text">
+				<h3>秒杀 马来西亚桂格燕麦饼干一盒162g口味随机</h3>
+				<p>
+					<span class="tag" style="background: #FF5555;">4.7折</span>
+				  <dfn class="index_price">¥ <span class="price_box">7.80</span></dfn>
+				  <del class="del_price">8.90</del>
+				  <p class="shopcar"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></p>
+				</p>
+  				</div>
+    		</li>
+    	</ul>
 	</div>
 </template>
 <script>
@@ -43,6 +94,10 @@ import css from '../bootstrap/css/bootstrap.css'
 	}
 </script>
 <style scoped>
+	*{
+		margin: 0;
+		padding: 0;
+	}
 	.portal-header{
 	height: 50px;
     padding:10px 80px;
@@ -81,11 +136,90 @@ import css from '../bootstrap/css/bootstrap.css'
 		height: 210px;
 		background-size: 100%;
 	}
+	.protal-icon{
+
+	}
 	.protal-icon li{
 	float: left;
     width: 20%;
     padding: 10px 0;
     list-style: none;
     text-align: center;
+    border-bottom: 3px solid lightgrey;
+	} 
+	.protal-icon li img{
+		width: 40px;
+		height: 40px;
+		display: block;
+	border: 0;
+    position: relative;
+    left: 22%;
+    margin-bottom: 10px;
+	}
+	.extension-item li img{
+		height: 175px;
+		width: 100%;
+		border-bottom: 3px solid lightgrey;
+	}
+	.index_list li{
+	border-bottom: 5px solid #f1f1f1;
+    overflow: hidden;
+    padding: 10px;
+    position: relative;
+	}
+	.index_list li img{
+		float: left;
+		width: 100px;
+		height: 100px;
+		margin-right: 10px;
+	}
+	.index_list li .index_text{
+		overflow: hidden;
+	    font-size: 14px;
+	    color: #959595;
+	}
+	.index_text h3{
+		overflow: hidden;
+	    font-size: 14px;
+	    color: #252525;
+	    margin-bottom: 20px;
+	}
+	.tag {
+	    padding: 0 3px;
+	    margin-right: 3px;
+	    border-radius: 2px;
+	    color: #fff;
+	    vertical-align: 3px;
+	}
+	.index_price {
+	    min-width: 70px;
+	    white-space: nowrap;
+	    display: inline-block;
+	    font-style: normal;
+	    font-size: 20px;
+	    color: #f55;
+	}
+	.index_list li .del_price{
+    color: #b4b4b4;
+   	font-size: 12px;
+	}
+	.shopcar{
+		position: absolute;
+	    right: 15px;
+	    bottom: 5px;
+	    width: 32px;
+	    height: 32px;
+	    box-sizing: content-box;   
+	}
+	.shopcar span{
+		display: block;
+	    width: 100%;
+	    height: 100%;
+	    line-height: 32px;
+	    background: #f55;
+	    border-radius: 50%;
+	    color: #fff;
+	    font-size: 16px;
+	    text-align: center;
 	}
 </style>
