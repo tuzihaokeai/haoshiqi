d17052fe1b044bc076ee390a1b56ae5405e125a2
hy <2710998134@qq.com>
2017-03-28 16:40:54 +0800

    hanyue

---
+++ hsq_front/src/store.js
@@ -0,0 +1,49 @@
+import Vue from "vue";
+import Vuex from "vuex";
+Vue.use(Vuex);  
+
+const store= new Vuex.Store({
+//con
+	state:{
+		datalist:[] ,//定义组件中的"状态"
+		titletext:[]
+	},
+   
+	actions:{
+		ADD_ITEM_ACTION:(store,data)=>{
+			//对于这个数据进行二次加工，或者是根据这个数据去后台请求数据
+
+			//console.log(data)
+			//提交到mutation 
+			store.commit("ADD_ITEM_MUTATION",data);
+			
+		}
+	},
+
+	mutations:{
+
+		ADD_ITEM_MUTATION:(state,data)=>{
+			//操作state-》增加数据
+			state.datalist.push(data.info.list);
+			state.titletext.push(data.title);
+				console.log(state.datalist);
+				console.log(state.title)
+		}
+	},
+
+	getters:{
+		//可以过滤,
+
+		//共享计算属性
+		complete:(state)=>{
+			return state.datalist;
+		},
+		uncomplete:(state)=>{
+			return state.datalist.filter(item=>item.marked==false).length;
+		}
+	}
+
+})
+
+
+export default store;
