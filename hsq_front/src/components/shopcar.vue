8d943a8bbff0b488024ed4783dcae3e7915b4844
katherine <820242547@qq.com>
2017-03-30 15:35:43 +0800

    liudi

--- hsq_front/src/components/shopcar.vue
+++ hsq_front/src/components/shopcar.vue
@@ -2,7 +2,7 @@
 	<div id="shopcar">
 		<div class="Header">
 			购物车
-			<span class="edit">编辑</span>
+			<span class="edit" @click="editClick()">{{edit}}</span>
 		</div>
 		<div class="none" v-if="show">
 			空空如也...
@@ -12,25 +12,25 @@
 		<div class="content" v-else>
 			<dl v-for="(data,index) in datalist">
 				<dt>
-					<input type="checkbox" v-model="flag" /><span>{{data.shopname}}</span><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+					<input type="checkbox" v-model="data.isChecked" @change="handleChange(data)"/><span>{{data.shopname}}</span><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 				</dt>
-				<dd>
-					<input type="checkbox" class="choose" :value="data" checked v-model="arr"/>
+				<dd v-for="(childdata,childindex) in data.children">
+					<input type="checkbox" class="choose"   v-model="childdata.isChecked" @change="handleChildChange(data)"/>
 					<div class="goodsdetail">
 						<p class="imgp" @click="changePage">
-							<img :src="data.goodsimg"/>
-							<span>{{data.goodsname}}</span>
+							<img :src="childdata.goodsimg"/>
+							<span>{{childdata.goodsname}}</span>
 						</p>
 						<p class="jisuanprice">
-							<span class="nowprice">￥<b>{{data.newprice}}</b></span>
-							<del>￥{{data.oldprice}}</del>
-							<span class="shuliang">
-								<a @click="delnumber">-</a>
-								<input type="text" v-model="number" class="numberInput"/>
-								<a @click="addnumber">+</a>
+							<span class="nowprice">￥<b>{{childdata.newprice}}</b></span>
+							<del>￥{{childdata.oldprice}}</del>
+							<span class="shuliang" v-show="xianshi">
+								<a @click="delnumber(childdata,childdata.goodsID)">-</a>
+								<input type="text" v-model="childdata.number" ref="numberInput" @input="handleClick" class="numberInput"/>
+								<a @click="addnumber(childdata,childdata.goodsID)">+</a>
 							</span>
 						</p>
-						<p class="surplus">剩余{{data.overgoods}}件</p>
+						<p class="surplus">剩余{{childdata.overgoods}}件</p>
 					</div>
 				</dd>
 			</dl>
@@ -42,14 +42,14 @@
 		
 		<div class="bottom">
 			<div class="allChoose">
-				<input type="checkbox" @click="allChoose()" ref="allchooseInput" v-model="flag"/>
+				<input type="checkbox" @change="allChoose()" :checked="beixuan"/>
 				<p>全选</p>
 			</div>
-			<div class="allPrice">
+			<div class="allPrice" v-show="xianshi">
 				<p>合计:  <span>￥{{sum}}</span></p>
 				<p>不包含运费</p>
 			</div>
-			<input type="button" value="去结算" class="gosum"/>
+			<input type="button"  @click="delClick()" :value="gosum" class="gosum"/>
 		</div>
 	</div>
 </template>
@@ -65,49 +65,178 @@ import router from "../router"
 			return{
 				datalist:[],
 				show:false,
+				xianshi:true,
 				number:1,
-				flag:true,
-				arr:[]
+				beixuan:false,
+				arr:[],
+				edit:"编辑",
+				gosum:"去结算",
+				status:"ok"
 				
 			}
 		},
 		methods:{
+		
+			editClick(){
+				if(this.edit=="完成"){
+					this.edit="编辑"
+					this.gosum = "去结算"
+					this.xianshi=true
+				}else{
+					this.edit="完成"
+					this.gosum = "删除"
+					this.xianshi=false
+					
+				}
+				
+			},
+			handleClick(){
+				console.log("00")
+			},
+			delClick(){
+				
+				if(this.gosum=="删除"){
+					
+					var newArr= []
+					console.log(this.datalist)
+					this.datalist.forEach((item)=>{
+						 newArr =[...newArr,...item.children.filter(obj=>obj.isChecked==true)];
+                		
+					})
+					var idArr= [];
+					
+					newArr.forEach((item)=>{
+						idArr = [...idArr,item.goodsID]
+					})
+				    console.log(idArr)
+				   
+					this.$http.post("http://localhost/php/delShopcar.php",{
+					username:Cookie.getCookie("userID"),
+					goodsID:idArr
+					}).then(res=>{
+						console.log(res)
+						this.$http.post("http://localhost/php/getShopcar.php",{
+							username:Cookie.getCookie("userID")
+							}).then(res=>{
+								console.log(res.body)
+								var newRes= [];
+								var quchonghArr = res.body.map(item=>item.shopname);
+								var set = new Set(quchonghArr);
+								quchonghArr = Array.from(set);
+			
+								
+								quchonghArr.forEach((item,index)=>{
+									newRes.push({shopname:item,isChecked:false,children:res.body.filter(obj=>obj.shopname==item)});
+								})
+								
+								this.datalist=newRes;
+								
+							},error=>{
+								
+						})
+						
+					},error=>{
+						
+				})
+				}
+				
+			},
 			changePage(id){
 				router.push(`index/detail/${id}`)
 			},
-			addnumber(){
-				this.number++
+			addnumber(data,id){
+				data.number++
+				//console.log(this.number)
+				console.log(id)
+				this.$http.post("http://localhost/php/updateShopcar.php",{
+					username:Cookie.getCookie("userID"),
+					goodsID:id,
+					number:data.number
+				}).then(res=>{
+					console.log(res)
+				},error=>{
+					
+				})
 			},
-			delnumber(){
-				if(this.number==1){
-					return
+			delnumber(data,id){
+				if(data.number==1){
+					return;
 				}else{
-					this.number--
+					data.number--
+					this.$http.post("http://localhost/php/updateShopcar.php",{
+					username:Cookie.getCookie("userID"),
+					goodsID:id,
+					number:data.number
+				}).then(res=>{
+					console.log(res)
+				},error=>{
+					
+				})
 				}
 			},
 			allChoose(){
-				console.log(this.$refs.allchooseInput)
-				if(this.flag){
-					this.flag=false
+				if(this.beixuan==true){
+					this.beixuan=false
+					console.log(this.beixuan)
+					this.datalist.forEach((item)=>{
+					console.log(item)
+					item.isChecked = false,
+					item.children.forEach((childrenitem)=>{
+						childrenitem.isChecked=false
+					})
+				})
 				}else{
-					this.flag=true
+					this.beixuan=true
+					console.log(this.beixuan)
+				this.datalist.forEach((item)=>{
+					console.log(item)
+					item.isChecked = true,
+					item.children.forEach((childrenitem)=>{
+						childrenitem.isChecked=true
+					})
+				})
 				}
+
 				
 			
+			},
 			
+			
+			handleChange(data){
+				
+				data.children.forEach((item)=>{
+					item.isChecked = data.isChecked;
+				})
+				
+				//console.log(data.children);
+			},
+			
+			handleChildChange(data){
+				var isChecked = data.children.every(item=>item.isChecked==true);
+				
+				data.isChecked = isChecked;
 			}
+		},	
+		
+		updated(){
+			//console.log("kerwi");
 		},
 		
             computed:{
                
                 sum:function(){
-                    var pricezhi=0
-				for(var i=0;i<this.arr.length;i++){
-					pricezhi+=parseFloat(this.arr[i].newprice)	
-				}
-				pricezhi = ShopcarFilter(pricezhi)
-				
-				return pricezhi
+                	//console.log(11); 
+                	
+                	var allprice = 0;
+                	this.datalist.forEach((item)=>{
+                		var newarr = item.children.filter(obj=>obj.isChecked==true);
+                		newarr.forEach((obj)=>{
+                			allprice+=obj.newprice*obj.number;
+                		})
+                	})
+                	
+                	return allprice.toFixed(2);
+                 	
                 }
             },
 
@@ -116,11 +245,14 @@ import router from "../router"
 				$.post("http://localhost/php/getShopcar.php",{
 						username:Cookie.getCookie("userID"),
 				}).then(res=>{
-					var arr=JSON.parse(res)
+					var arr=JSON.parse(res);
+					arr.map((item)=>{
+						item.isChecked = false;
+					})
 					console.log(arr)
-					this.datalist=arr
 					
-					var res=[
+					
+					var newres=[
 					
 					];
 //					var shopnameArr = []
@@ -137,25 +269,38 @@ import router from "../router"
 //						
 //					}
 //					console.log(shopnameArr)
-//					console.log(quchonghouArr)
-					for(var i=0;i<arr.length;i++){
-						
-						if(arr.indexOf(arr[i].shopname)==-1){
-							console.log(arr.indexOf(arr[i].shopname))
-							var obj={
-								shopname:"",
-								goods:[]
-							}
-							
-							obj.shopname=arr[i].shopname
-							obj.goods.push(arr[i])
-							res.push(obj);							
-						}else{
-							console.log(arr.indexOf(arr[i].shopname))
-							res[arr.indexOf(arr[i].shopname)].goods.push(arr[i])
-						}
-					}
-					console.log(res)
+
+				    var quchonghouArr = arr.map(item=>item.shopname);
+					var set = new Set(quchonghouArr);
+					quchonghouArr = Array.from(set);
+
+					
+					quchonghouArr.forEach((item,index)=>{
+						newres.push({shopname:item,isChecked:false,children:arr.filter(obj=>obj.shopname==item)});
+					})
+					
+					this.datalist=newres;
+//					for(var i=0;i<arr.length;i++){
+//						
+//						if(arr.indexOf(arr[i].shopname)==-1){
+//							//console.log(arr.indexOf(arr[i].shopname))
+//							var obj={
+//								shopname:"",
+//								goods:[]
+//							}
+//							
+//							obj.shopname=arr[i].shopname
+//							obj.goods.push(arr[i])
+//							res.push(obj);							
+//						}else{
+//							console.log(arr.indexOf(arr[i].shopname))
+//							res[arr.indexOf(arr[i].shopname)].goods.push(arr[i])
+//						}
+//					}
+					//console.log(res)
+					
+					
+					
 					
 				
 					if(this.datalist.length){
@@ -349,7 +494,7 @@ html,body{
 		color:white;
 		flex: 3;
 		background: #f55;
-		width:120px;
+		/*width:120px;*/
 		height:49px;
 	}
 </style>
